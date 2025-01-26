30cd182388dd19b469e889bd7921c3759c397991
Adrian Pionka <adrian@pionka.com>
2025-01-26 09:37:30 +0100

    add GlobalExceptionHandler

---
+++ src/main/resources/archetype-resources/src/main/java/infrastructure/rest/handler/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package ${package}.infrastructure.rest.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle(Exception ex) {
+        log.error("An unexpected error occurred", ex);
+        return handleError(HttpStatus.INTERNAL_SERVER_ERROR, List.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.warn("Validation error: {}", ex.getMessage());
+        Map<String, List<String>> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                ));
+
+        return handleError(HttpStatus.BAD_REQUEST, errors);
+    }
+
+    private ResponseEntity<Object> handleError(HttpStatus status, Object errors) {
+        final var body = new LinkedHashMap<String, Object>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, status);
+    }
+}
